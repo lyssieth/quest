dadffc6950497393fc8c2d784c30ed7c150b6e95
Lyssieth <lys@lys.ee>
2023-05-12 16:20:24 +0300

    Add some extra stuff, a basic landing pade.
    Oh and dark mode. Did I forget about mentioning that?
    Or, I just did. Anyways, let's just forget.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>
+	Welcome to the eventual forever-home of <a href="https://github.com/lyssieth/quest">quest</a>...
+	perhaps.
+</h1>
+
+<p>
+	Quest is going to be a platform for interactive fiction writing. Name may change, but the spirit
+	will not.
+</p>
+<p>The spirit being that it's developed by a person with intense ADD.</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import adapter from 'svelte-adapter-deno';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 /** @type import('@sveltejs/kit').Config */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			scss: {
+				prependData: `@import 'src/styles/variables.scss' as *;`
+			}
+		})
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
