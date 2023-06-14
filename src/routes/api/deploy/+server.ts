162684de8518ed4b02af1a66db9b0f262390bf4c
Lyssieth <lys@lys.ee>
2023-06-14 03:18:07 +0300

    Write some words
    Okay, in reality I wanna make the delpoy thing work

--- src/routes/api/deploy/+server.ts
+++ src/routes/api/deploy/+server.ts
@@ -1,11 +1,39 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const POST = (async ({ request }) => {
-	const content = await request.json();
+const WORKFLOW_NAME = 'Build and Deploy Docker Image';
 
-	console.debug('POST /api/deploy', content);
+export const POST = (async ({ request }: { request: Request }) => {
+	const content = (await request.json()) as WorkflowPost;
+
+	if (content.workflow_run.name !== WORKFLOW_NAME) {
+		console.debug('Ignoring workflow run', content.workflow_run.name);
+		return new Response(null, {
+			status: 200
+		});
+	}
+
+	if (content.action !== 'completed') {
+		console.debug('Ignoring workflow run', content.action);
+
+		return new Response(null, {
+			status: 200
+		});
+	}
+
+	console.debug('Deploying new version... somehow');
+	// TODO: Figure out how to deploy new version
+	//   Since we're running in a Docker container
+	//   we gotta somehow let the host system know?
 
 	return new Response(null, {
 		status: 200
 	});
 }) satisfies RequestHandler;
+
+interface WorkflowPost {
+	action: string;
+	workflow_run: {
+		name: string;
+		status: string;
+	};
+}
